393059f7c8c612de90a0b74e03d37d9bc7780e92
shubham <shubhampatil97144@gmail.com>
2023-02-13 16:40:49 +0530

    implement fetch data

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ShowValue from './Components/ShowValue';
+
+function App() {
+  return (
+    <div className="App">
+      <ShowValue/>
+    </div>
+  );
+}
+
+export default App;
